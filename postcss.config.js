66b948315dbcae708c339070d55a06861103d9c8
Diego <devdiegoferreira@gmail.com>
2023-02-10 18:47:04 -0300

    Remove duplicated dependencies

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,6 @@
 export default {
   plugins: {
-    autoprefixer: {},
-    'postcss-custom-media': {},
     'postcss-import': {},
-    'postcss-media-minmax': {},
     'postcss-simple-vars': {},
     'postcss-mixins': {},
     'postcss-preset-env': { stage: 1 }
